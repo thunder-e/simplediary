be635b917509125e3371103b707549c9e20e2847
thunder-e <79903353+thunder-e@users.noreply.github.com>
2022-09-30 16:48:44 +0900

    🚧 사용자 입력 처리 & 🔥 사용하지 않는 기본 파일 삭제

---
+++ src/DiaryEditor.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+  //   [state, 상태변화함수]          //초기값
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value, //변경하고자 하는 프로퍼티를 나중에
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장성공!");
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        오늘의 감정점수 :{" "}
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
