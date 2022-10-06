cec704d8ebdfebdf64daaab5bcbe69ab0ecd6ef9
thunder-e <79903353+thunder-e@users.noreply.github.com>
2022-10-06 10:58:01 +0900

    ✨ 최적화 : useCallback  🐛Strict 모드 삭제(콘솔에 두번씩 출력문제)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
@@ -6,7 +6,7 @@ import DiaryList from "./DiaryList";
 //import Lifecycle from "./Lifecycle";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([]); //[] App component, DiaryEditor 한번씩 렌더링
 
   const dataId = useRef(0);
 
@@ -25,14 +25,15 @@ function App() {
       };
     });
 
-    setData(initData);
+    setData(initData); // dataState가 한번 더 렌더링
   };
 
   useEffect(() => {
     getData();
   }, []); // 빈배열 -> 콜백함수는 앱컴포넌트가 탄생(mount)되는 시점에 실행됨
 
-  const onCreate = (author, content, emotion) => {
+  const onCreate = useCallback((author, content, emotion) => {
+    //useCallback : 함수의 재생성
     const created_date = new Date().getTime();
     const newItem = {
       author,
@@ -43,21 +44,20 @@ function App() {
     };
     dataId.current += 1;
 
-    setData([newItem, ...data]);
-  };
+    setData((data) => [newItem, ...data]); //함수형 업데이트 : 상태변화 함수(setState)에 함수를 전달 -> 항상 최신의 state값을 받아올 수 있음
+  }, []); //onCreate에 useCallback을 사용하고 의존성 배열을 빈 값으로 두면 mount시에 한번만 실행되기 때문에 data state가 초기값인 빈 배열인 상태 => 해결위해 함수형 업데이트를 사용
 
-  const onRemove = (targetId) => {
-    const newDiaryList = data.filter((it) => it.id !== targetId);
-    setData(newDiaryList);
-  };
+  const onRemove = useCallback((targetId) => {
+    setData((data) => data.filter((it) => it.id !== targetId)); // 최신 state를 이용하기 위해서는 함수형 업데이트에 인자부분과 return 부분에 data를 사용해야 한다.
+  }, []);
 
-  const onEdit = (targetId, newContent) => {
-    setData(
+  const onEdit = useCallback((targetId, newContent) => {
+    setData((data) =>
       data.map((it) =>
         it.id === targetId ? { ...it, content: newContent } : it
       )
     );
-  };
+  }, []);
 
   // 연산 최적화 = useMemo
   // useMemo를 사용하는 순간 함수가 아닌 getDiaryAnalysis는 값을 리턴받게 됨
@@ -72,8 +72,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Lifecycle /> */}
-      {/* <OptimizeTest /> */}
       <DiaryEditor onCreate={onCreate} />
       <div>전체 일기 : {data.length}</div>
       <div>기분 좋은 일기 개수 : {goodCount}</div>
